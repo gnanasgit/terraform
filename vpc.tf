d53bd999eeb7ae543f90fa296bf578521e9f9c08
gnanasgit <gnanasgit@gmail.com>
2023-11-14 14:48:24 +0000

    create public subnet-zone-A

--- vpc.tf
+++ vpc.tf
@@ -8,4 +8,13 @@ resource "aws_vpc" "tf-ecomm" {
  }  
 }
 
+#Create pub subnet - AZ - A
+resource "aws_subnet" "tf-ecomm-pub-sn-A" {
+  vpc_id     = aws_vpc.tf-ecomm.id
+  cidr_block = "192.168.0.0/24"
+  availability_zone = "us-east-2a"
 
+  tags = {
+    Name = "ecomm-public-subnet-A"
+  }
+}
